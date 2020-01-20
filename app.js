f09a3400d0b663032b11046ddfab8ae6a860213c
m.dias <moises.dias16@outlook.com>
2020-01-19 23:57:04 -0300

    youtube links

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("helloasdf aaa")
+const linkCategory = document.querySelector("#linkCategory");
+
+let linkCategories = [];
+
+linkCategory.addEventListener('keydown', function (event) {
+    if(event.keyCode === 188) {
+        event.preventDefault();
+        linkCategories.push(linkCategory.value);
+        console.log(linkCategories)
+        linkCategory.value = '';
+
+        displayLinkCategories();
+    }
+})
+
+function displayLinkCategories() {
+    console.log("displaying link categories");
+}
